266b507b77d67c376006b003f6c6b406bc91b786
Brennan Chesley <holtchesley@example.com>
2017-09-16 00:41:46 -0400

    Ensure Testing works with adverse events.
    The generation of adverse events was running into several bugs in the
    code that was preventing testing from running. This patch resolves that,
    though not all adverse events are completely implemented yet.

--- src/events.py
+++ src/events.py
@@ -1,7 +1,7 @@
 from copy import copy
 from node import DownNode
 
-class Event:
+class Event(object):
     """
     The Base Event Class. Includes shared behavior.
     """
@@ -14,7 +14,7 @@ class Event:
         return self.event_map['start_time'] == other.event_map['start_time']
     def get_start_time(self):
         return self.event_map['start_time']
-    def reverse(self): return []
+    def backout(self): return []
     def window_terminus(self):
         """
         Returns a new event_map with the start at the end of this one, and no event length.
@@ -33,26 +33,36 @@ class NetworkEvent(Event):
     def handle(self,nodes,network_broker): pass
 
 class DeliveryDrop(NetworkEvent):
-    def __init__(self,event_map): super().__init__(event_map)
-    def reverse(self): return StopDeliveryDrop(self.window_terminus())
-class StopDeliveryDelay:
+    def __init__(self,event_map):
+        super().__init__(event_map)
+    def backout(self): return [StopDeliveryDrop(self.window_terminus())]
+class StopDeliveryDrop(NetworkEvent):
     def __init__(self,event_map): super().__init__(event_map)
 
 class DeliveryDelay(NetworkEvent):
-    def __init__(self,event_map): super().__init__(event_map)
-    def reverse(self): return StopDeliveryDelay(self.window_terminus())
-class StopDeliveryDelay:
-    def __init__(self,event_map): super().__init__(event_map)
+    def __init__(self,event_map):
+        super().__init__(event_map)
+
+    def prn(self):
+        print(self.event_map)
+
+    def backout(self):
+        return [StopDeliveryDelay(self.window_terminus())]
+    
+
+class StopDeliveryDelay(NetworkEvent):
+    def __init__(self,event_map):
+        super().__init__(event_map)
 
 
 class ReceiveDrop(NetworkEvent):
     def __init__(self,event_map): super().__init__(event_map)
-    def reverse(self): return StopDeliveryDrop(self.window_terminus())
+    def backout(self): return [StopDeliveryDrop(self.window_terminus())]
     def handle(self,nodes,network_broker):
-        for to_id in self.event_map['affected_nodes']:
+        for to_node in self.event_map['affected_nodes']:
             for from_node in nodes:
                 network_broker['connections'].discard((from_node,to_node))
-class StopReceiveDrop:
+class StopReceiveDrop(NetworkEvent):
     def __init__(self,event_map): super().__init__(event_map)
     def handle(self,nodes,network_broker):
         for to_id in self.event_map['affected_nodes']:
@@ -65,12 +75,12 @@ class TransmitDrop(NetworkEvent):
     TransmitDrop represents all packets sent between two nodes being dropped.
     """
     def __init__(self,event_map): super().__init__(event_map)
-    def reverse(self): return StopTransmitDrop(self.window_terminus())
+    def backout(self): return [StopTransmitDrop(self.window_terminus())]
     def handle(self,nodes,network_broker):
         pair = self.event_map['affected_node_pair']
         network_broker['connections'].discard(pair)
 
-class StopTransmitDrop:
+class StopTransmitDrop(NetworkEvent):
     def __init__(self,event_map): super().__init__(event_map)
     def handle(self,nodes,network_broker):
         pair = self.event_map['affected_node_pair']
@@ -82,18 +92,20 @@ class DeliveryDuplicate(NetworkEvent):
     DeliveryDuplicates Represents all messaages that a node attempts to deliver being duplicated.
     """
     def __init__(self,event_map): super().__init__(event_map)
-    def reverse(self): return StopDeliveryDuplicate(self.window_terminus())
+    def backout(self): return [StopDeliveryDuplicate(self.window_terminus())]
     def handle(self,nodes,network_broker):
         from_node = self.event_map['affected_node']
         for to_node in nodes:
-            network_broker['duplicates'][(from_node,to_node)] += 1
+            if from_node != to_node:
+                network_broker['duplicates'][(from_node,to_node)] += 1
 
-class StopDeliveryDuplicate:
+class StopDeliveryDuplicate(NetworkEvent):
     def __init__(self,event_map): super().__init__(event_map)
     def handle(self,nodes,network_broker):
         from_node = self.event_map['affected_node']
         for to_node in nodes:
-            network_broker['duplicates'][(from_node,to_node)] = max(0,network_broker['duplicates'][(from_node,to_node)] -1)
+            if from_node != to_node:
+                network_broker['duplicates'][(from_node,to_node)] = max(0,network_broker['duplicates'][(from_node,to_node)] -1)
 
 class DeliverMessage(NetworkEvent):
     """
@@ -128,7 +140,7 @@ class PowerDown(PowerEvent):
     PowerDown Represents a node shutting down.
     """
     def __init__(self,event_map): super().__init__(event_map)
-    def reverse(self): return StopPowerDown(self.window_terminus())
+    def backout(self): return [StopPowerDown(self.window_terminus())]
     def handle(self,nodes,power_broker):
         node_id = self.event_map['affected_node']
         if node_id not in power_broker['down_nodes']:
@@ -166,84 +178,11 @@ class ClockSkew(TimerEvent):
     """
     def __init__(self,event_map): super().__init__(event_map)
     def handle(self,nodes,time_broker):
-        time_broker['node_time_offsets'][self.event_map['affected_node']] += self.event_map['delay']
+        time_broker['node_time_offsets'][self.event_map['affected_node']] += self.event_map['skew_amount']
 
 class HealTimer(TimerEvent):
     def __init__(self,event_map): super().__init__(event_map)
     def handle(self,nodes,time_broker):
         for node_id in time_broker['node_timers']:
             time_broker['node_timers'][node_id] = 0
-
-
-# class NetworkBroker(GenericStateMachine):
-#     """
-#     Broker to coordinate network traffic
-#     nodes = A map of node ids to node objects.
-#     network = An adjacency list of what nodes can talk to each other. If a is
-#         in network[b] than b -> a communcation is allowed. This is a map of
-#         id type -> set(id type)
-#     messages = A queue of messages. messages[0] is the head, where messages are
-#         sent from. Messages are tuples in the form of (from, to, data).
-#     """
-#     def __init__(self, nodes):
-#         self.nodes = nodes
-#         self.network = dict([(i, set(nodes.keys())) for i in nodes.keys()])
-#         self.messages = []
-#     def send_to(self, sender, to, data):
-#         self.messages.append((to, sender, data))
-#     def steps(self):
-#         #TODO(Wesley) Make more clear
-#         existing_edges = sum([[(a[0], b) for b in a[1]] for a in self.network.items()], [])
-#         broken_edges = sum([[(a[0], b) for b in (set(self.nodes.keys()) - a[1]) if a[0] != b] for a in self.network.items()], [])
-#         deliver_msg = tuple(just("DeliverMsg"))
-#         drop_msg = tuple(just("DropMsg"))
-#         duplicate_msg = tuple(just("DuplicateMsg"))
-#         delay_msg = tuple(just("DelayMsg"), integers(min_value=1, max_value=len(self.messages)))
-#         destroy_edge = tuple(just("DestroyEdge"), sampled_from(existing_edges))
-#         heal_edge = tuple(just("HealEdge"), sampled_from(broken_edges))
-#         actions = []
-#         if len(existing_edges) > 0:
-#             actions.append(destroy_edge)
-#         if len(broken_edges) > 0:
-#             actions.append(heal_edge)
-#         if len(self.messages) != 0:
-#             actions.append(deliver_msg)
-#             actions.append(drop_msg)
-#             actions.append(duplicate_msg)
-#             actions.append(delay_msg)
-#         return one_of(actions)
-#     def execute_step(self, step):
-#         """
-#         Actions:
-#         DeliverMsg
-#             If next message is deliverable, deliver it. Otherwise, drop it.
-#         DropMsg
-#             Drop the next message.
-#         DestroyEdge (from, to)
-#             Destroys the edge from -> to, causing any packets sent along it to be dropped.
-#         HealEdge (from, to)
-#             Heal the edge from -> to, allowing packets to be sent along it.
-#         DuplicateMsg
-#             Create a copy of the message at the front of the queue
-#         DelayMsg n
-#             Push the message at the front of the queue back by n slots
-#         """
-#         action = step[0]
-#         value = step[1:]
-#         if action == "DeliverMsg":
-#             message = self.messages.pop(0)
-#             self.nodes[message[1]].recv(message[0], message[2])
-#         if action == "DropMsg":
-#             self.messages.pop(0)
-#         if action == "DestroyEdge":
-#             self.network[step[0]].remove(step[1])
-#         if action == "HealEdge":
-#             self.network[step[0]].add(step[1])
-#         if action == "DuplicateMsg":
-#             self.messages.insert(0, self.messages[0])
-#         if action == "DelayMsg":
-#             self.messages.insert(value, self.messages.pop(0))
-
-
-
 
