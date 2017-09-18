a27e71157269e737eb2f3e83104d63f619f8728e
Brennan Chesley <holtchesley@example.com>
2017-09-18 09:19:46 -0400

    PEP8 Compliance for node.py
    Make node comply with pep8. Altered several uses of type to instead use
    isinstance to comply with pep8.

--- src/node.py
+++ src/node.py
@@ -1,23 +1,38 @@
+"""
+The actual implementation of raft.
+"""
+
 import math
 from message import AppendEntries, AppendEntriesResponse, RequestVote, RequestVoteResponse
 
 
 class DownNode:
-    def receive(self,sender,message):
+    """
+    DownNode is a mock node, intended to interoperate with the powerbroker to
+    represent a node that is powered down.
+    It's methods simply pass
+    """
+    def receive(self, sender, message):
+        "Downed nodes receive no messages"
         pass
 
     def timer_trip(self):
+        "Downed Nodes have no timers"
         pass
 
+# pylint: disable=too-many-instance-attributes
 class Node:
-    def __init__(self,node_id,conf,rng,broker):
+    """
+    The workhorse of this raft implementation.
+    """
+    def __init__(self, node_id, conf, rng, broker):
         self.node_id = node_id
         self.conf = conf
         self.rng = rng
         self.broker = broker
 
         self.term = 0
-        self.log = [] # list[tuple(term, entry)]
+        self.log = []  # list[tuple(term, entry)]
         self.commit_index = 0
         self.last_applied = 0
         self.voted_for = None
@@ -26,40 +41,46 @@ class Node:
         self.election_timeout = self.calculate_election_timeout()
 
     def calculate_election_timeout(self):
+        " TODO "
         return self.rng.randint(self.conf['election_timeout_window'][0],
                                 self.conf['election_timeout_window'][1])
 
     def is_candidate(self):
+        "TODO"
         return self.node_type == 'Candidate'
 
     def is_follower(self):
+        "TODO"
         return self.node_type == 'Follower'
 
     def is_leader(self):
+        "TODO"
         return self.node_type == 'Leader'
 
     def setup(self):
+        "Emulates a node booting up"
         self.broker.set_timeout(self.node_id, self.election_timeout)
 
-    def change_type(self, to):
+    def change_type(self, new_type):
         """
         Convert this node to a different type, and make any other needed state changes.
         """
-        # TODO: always log instead of logging when different?
-        if self.node_type != to:
+        #-TODO always log instead of logging when different?
+        if self.node_type != new_type:
             self.broker.log({'term': self.term,
                              'node_type': self.node_type,
-                             'to_type': to,
+                             'to_type': new_type,
                              'log_type': 'change_type'})
-        assert to == 'Follower' or to == 'Candidate' or to == 'Leader'
-        if to == 'Leader':
-            assert(self.node_type != 'Follower')
+        assert new_type == 'Follower' or new_type == 'Candidate' or new_type == 'Leader'
+        if new_type == 'Leader':
+            assert self.node_type != 'Follower'
 
-        self.node_type = to
-        if to == 'Follower' or to == 'Candidate':
+        self.node_type = new_type
+        if new_type == 'Follower' or new_type == 'Candidate':
             self.broker.set_timeout(self.node_id, self.election_timeout)
-        elif to == 'Leader':
-            self.broker.set_timeout(self.node_id, self.conf['heartbeat_timeout'])
+        elif new_type == 'Leader':
+            self.broker.set_timeout(
+                self.node_id, self.conf['heartbeat_timeout'])
 
     def update_term(self, term, new_candidate):
         """
@@ -77,54 +98,60 @@ class Node:
             self.voted_for = None
             self.election_timeout = self.calculate_election_timeout()
 
-    def receive(self,sender,message):
+    def receive(self, sender, message):
+        "TODO"
         self.update_term(message.term, False)
-        assert type(message) == AppendEntries or \
-               type(message) == RequestVote or \
-               type(message) == AppendEntriesResponse or \
-               type(message) == RequestVoteResponse
-        if type(message) == AppendEntries:
+        assert isinstance(message, (AppendEntries, RequestVote, AppendEntriesResponse,\
+                          RequestVoteResponse))
+        if isinstance(message, AppendEntries):
             if self.is_follower():
                 self.broker.set_timeout(self.node_id, self.election_timeout)
             elif self.is_candidate() or self.is_leader():
                 self.change_type('Follower')
             else:
                 # This case will fire when we add cluster config changes.
-                assert(False)
-        elif type(message) == RequestVote:
+                assert False
+        elif isinstance(message, RequestVote):
             if message.term < self.term or self.voted_for is not None:
-                self.broker.send_to(self.node_id, sender, RequestVoteResponse(self.term, False))
+                self.broker.send_to(self.node_id, sender,
+                                    RequestVoteResponse(self.term, False))
             else:
-                self.broker.send_to(self.node_id, sender, RequestVoteResponse(self.term, True))
-        elif type(message) == AppendEntriesResponse:
+                self.broker.send_to(self.node_id, sender,
+                                    RequestVoteResponse(self.term, True))
+        elif isinstance(message, AppendEntriesResponse):
             pass
-        elif type(message) == RequestVoteResponse:
+        elif isinstance(message, RequestVoteResponse):
             if message.vote_granted:
                 self.votes_received.add(sender)
-                if len(self.votes_received) > math.floor(len(self.conf['nodes'])/2):
+                if len(self.votes_received) > math.floor(len(self.conf['nodes']) / 2):
                     self.change_type('Leader')
 
     def timer_trip(self):
-        # TODO: this if statement was added to fix a "compile error". This was not thought through and may be
-        # wrong.
+        "TODO"
+        #-TODO: this if statement was added to fix a "compile error". This was not
+        # thought through and may be wrong.
         last_logged_term = -1
         last_logged_entry = None
-        if len(self.log) > 0:
+        if self.log:
             last_logged_term = self.log[-1][0]
             last_logged_entry = self.log[-1][1]
 
         if not self.is_leader():
             self.change_type('Candidate')
-            self.update_term(self.term+1, True)
+            self.update_term(self.term + 1, True)
             self.votes_received.add(self.node_id)
             self.voted_for = self.node_id
             for node in self.conf['nodes']:
                 if self.node_id != node:
                     self.broker.send_to(self.node_id, node,
-                        RequestVote(self.term, self.node_id, len(self.log), last_logged_term))
+                                        RequestVote(self.term, self.node_id,
+                                                    len(self.log), last_logged_term))
         else:
             for node in self.conf['nodes']:
                 if self.node_id != node:
                     self.broker.send_to(self.node_id, node,
-                        AppendEntries(self.term, self.node_id, len(self.log), last_logged_entry, [], self.commit_index))
-            self.broker.set_timeout(self.node_id, self.conf['heartbeat_timeout'])
+                                        AppendEntries(self.term, self.node_id,
+                                                      len(self.log), last_logged_entry,
+                                                      [], self.commit_index))
+            self.broker.set_timeout(
+                self.node_id, self.conf['heartbeat_timeout'])
